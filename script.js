36cd7347614a8a15ddf5c4d40c5fbb3648f0715a
otata0913 <62365310+otata0913@users.noreply.github.com>
2020-12-01 15:56:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const information=document.getElementById("information");
+const infoArray=[];
+
+infoArray.ブラウザのユーザーエージェント=navigator.userAgent;
+
+alert("【警告】お使いの端末は重大な問題を発生させるコンピュータウイルスに感染しています (アクセス端末情報) = " + navigator.userAgent);
+
+alert("あなたの端末からウイルスを取り除くため、ストレージ内のほとんどすべてのデータを削除しています...")
+
+alert("完了。")
